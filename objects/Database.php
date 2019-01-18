737f3d2051050a567faf92a9d41060f950925fce
Andrew H <FlamesRunner@users.noreply.github.com>
2019-01-18 17:46:30 -0500

    Update Database.php

--- objects/Database.php
+++ objects/Database.php
@@ -1,35 +1,30 @@
 <?php
     class Database {
-        protected $DBname;
-        protected $DBuser;
-        protected $DBpass;
-        protected $DBhost;
-        protected $DBchar;
-        protected $DBoptions;
-        protected $DSN;
-        public $PDO;
+        private $DBname;
+        private $DBuser;
+        private $DBpass;
+        private $DBhost;
+        private $DBchar;
+        private $DBoptions;
+        private $DSN;
+        private $PDO;
         
-        public function _construct() {
-            $this->$DSN = "mysql:host=$DBhost;DBname=$DBname;charset=$DBchar";
-            $this->$DBname = 'PHP-Employee-Manager-O';
-            $this->$DBuser = 'root';
-            $this->$DBpass = '';
-            $this->$DBhost = 'localhost:8080';
-            $this->$DBchar = 'utf8mb4';
-            $this->DBoptions = array(
-                PDO::ATTR_PERSISTENT         => true,
-                PDO::ATTR_EMULATE_PREPARES   => false,
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ); 
+        public function _construct($DSN, $DBname, $DBuser, $DBpass, $DBhost, $DBchar, $DBoptions) {
+            $this->DSN = $DSN;
+            $this->DBname = $DBName;
+            $this->DBuser = $DBUser;
+            $this->DBpass = $DBPass;
+            $this->DBhost = $DBHost;
+            $this->DBchar = $DBchar;
+            $this->DBoptions = $DBoptions;
         }
 
         public function PDO() {
-            if ($this->$PDO) {
-                return $this->$PDO;
+            if ($this->PDO) {
+                return $this->PDO;
             } else {
                 try {
-                    $this->$PDO = new PDO($DSN, $DBuser, $DBpass, $options);
+                    $this->PDO = new PDO($this->DSN, $this->DBuser, $this->DBpass, $this->options);
                 } catch (PDOException $error) {
                     return null;
                     echo 'error';
