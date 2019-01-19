b15ec45f514b0518fb91586ce2ff4c7155701203
Andrew H <FlamesRunner@users.noreply.github.com>
2019-01-19 15:14:07 -0500

    Fixed some stuff, added suggestions

--- scripts/Create.php
+++ scripts/Create.php
@@ -1,8 +1,9 @@
 <?php
+    require '../config.php';
+    require '../objects/Database.php';
     if (isset($_POST['employee-submit'])) {
-        include_once 'Database.php';
-        $Database = new Database;
-        $PDO = $Database->PDO();
+        $database = new Database(SQLDB, SQLUSER, SQLPASS, SQLHOST, SQLCHAR, SQLOPTIONS);
+        $PDO = $database->PDO();
 
         $username = $_POST['username'];
         $firstname = $_POST['firstname'];
@@ -19,15 +20,23 @@
         // Error Handlers
 
         if (preg_match("/\s+/", $username)) {
+            // The regular expression you used here only looks for spaces in the username.
+            // Ideally, you want to be checking it to determine whether or not it is alphanumeric, which can be accomplished with:
+            // /[a-zA-Z0-9]+/
+            // See https://gist.github.com/cferdinandi/6718463
+            // Simply insert it into the double quotes before $username.
             header("Location: ../pages/create_page.php?error=username");
             exit();
         } else if (!preg_match("/[a-zA-Z]*/", $firstname) || !preg_match("/[a-zA-Z]*/", $middlename) || !preg_match("/[a-zA-Z]*/", $lastname)) {
+            // By adding an asterisk, you essentially allow the user to create a blank username (zero characters or more)
+            // Replace it with + to require at least one of the characters [a-zA-Z].
+            // This is the same for the rest of them. At your discretion, you may allow the user to keep their middle name blank.
             header("Location: ../pages/create_page.php?error=name");
             exit();
         } else {
             $availablePositions = $PDO->query("SELECT `position` FROM `users`")->fetchAll();
             array_unique($availablePositions);
-            if (!in_array($position, $availablePositions)) {
+            if (!in_array($position, $availablePositions)) { // Nothing happens apart from you setting $newPosition. 
                 $newPosition = true;
             } else {
                 $newPosition = false;
